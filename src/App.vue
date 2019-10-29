0fc5f912a4aa4adad56db62257c1e31c2d658347
aymy <371999562@qq.com>
2019-10-29 09:59:23 +0800

    新增练习文件夹

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<!--<style lang="less">
+@nice-blue: #5B83AD;
+@light-blue:@nice-blue;
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background:@light-blue;
+}
+
+
+</style>-->
+<style lang="scss">
+$nice_blue:red;
+$light_blue:$nice_blue;
+#app{
+  background:$light_blue;
+}
+
+
+</style>
